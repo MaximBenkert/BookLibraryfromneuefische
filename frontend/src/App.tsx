4a4aacf6bc03c40c7074eaf97781536747a374e4
MaximBenkert <126075845+MaximBenkert@users.noreply.github.com>
2023-03-28 16:17:17 +0200

    frontend add Book

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import axios from "axios";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hallo</h1>
+
+    </div>
+  );
+}
+
+export default App;
